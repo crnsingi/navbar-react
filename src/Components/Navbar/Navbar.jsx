d70e0531d6efa43118e7682e9fe82a5e610eb26d
Cesar <cesarnsingi@hotmail.com>
2025-02-25 19:34:36 +0200

    set navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,6 +6,13 @@ const Navbar = () => {
    <header className="header">
     <a href="/" className="logo">Logo</a>
 
+    <nav className="navbar">
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Portfolio</a>
+        <a href="/">Services</a>
+        <a href="/">Contact</a>
+    </nav>
    </header>
   )
 }
